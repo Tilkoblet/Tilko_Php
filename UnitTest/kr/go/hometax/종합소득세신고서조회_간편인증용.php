d5b4b766f3b02033be2ee29c22bd699ecc91852a
Seongwon Seo <seongwon.seo@tilkoblet.com>
2022-04-29 17:03:34 +0900

    .

---
+++ UnitTest/kr/go/hometax/종합소득세신고서조회_간편인증용.php
@@ -0,0 +1,122 @@
+<?php
+
+set_include_path(get_include_path() . PATH_SEPARATOR . 'D:/php/phpseclib1.0.19');
+require_once('Crypt/RSA.php');
+
+$apiHost   = "https://api.tilko.net/";
+$apiKey    = "";
+
+
+// AES 암호화 함수
+function aesEncrypt($aesKey, $aesIv, $plainText) {
+    $ret = openssl_encrypt($plainText, 'AES-128-CBC', $aesKey, OPENSSL_RAW_DATA, $aesIv);	//default padding은 PKCS7 padding
+    return base64_encode($ret);
+}
+
+
+// RSA 공개키(Public Key) 조회 함수
+function getPublicKey($apiKey) {
+    global $apiHost;
+
+    $url        = $apiHost . "api/Auth/GetPublicKey?APIkey=" . $apiKey;
+
+    $curl       = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL             => $url,
+        CURLOPT_RETURNTRANSFER  => true,
+        CURLOPT_CUSTOMREQUEST   => "GET",
+        CURLOPT_SSL_VERIFYHOST  => 0,
+        CURLOPT_SSL_VERIFYPEER  => 0
+    ));
+
+    $response   = curl_exec($curl);
+
+    curl_close($curl);
+
+    return json_decode($response, true)["PublicKey"];
+}
+
+
+// RSA Public Key 조회
+$rsaPublicKey   = getPublicKey($apiKey);
+print("rsaPublicKey:" . $rsaPublicKey);
+
+
+// AES Secret Key 및 IV 생성
+$aesKey     = random_bytes(16);
+$aesIv      = str_repeat(chr(0), 16);
+
+
+// AES Key를 RSA Public Key로 암호화
+$rsa            = new Crypt_RSA();
+$rsa->loadKey($rsaPublicKey);
+$rsa->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+
+$aesCipheredKey = $rsa->encrypt($aesKey);
+
+
+// API URL 설정
+// HELP: https://tilko.net/Help/Api/POST-api-apiVersion-HometaxSimpleAuth-UTERNAAZ110-JongHabSoDeugSe-SinGo
+$url        = $apiHost . "api/v1.0/HometaxSimpleAuth/UTERNAAZ110/JongHabSoDeugSe/SinGo";
+
+
+// 간편인증 요청 후 받은 값 정리
+$reqData = array(
+    "CxId"                      => "",
+    "PrivateAuthType"           => "",
+    "ReqTxId"                   => "",
+    "Token"                     => "",
+    "TxId"                      => "",
+    "UserName"                  => "",
+    "BirthDate"                 => "",
+    "UserCellphoneNumber"       => "",
+);
+
+
+// API 요청 파라미터 설정
+$headers    = array(
+    "Content-Type:"             . "application/json",
+    "API-Key:"                  . $apiKey,
+    "ENC-Key:"                  . base64_encode($aesCipheredKey),
+);
+
+// 위에서 정의한 reqData 객체를 활용해보세요.
+$bodies     = array(
+    "CxId"                      => $reqData["CxId"],
+    "PrivateAuthType"           => $reqData["PrivateAuthType"],
+    "ReqTxId"                   => $reqData["ReqTxId"],
+    "Token"                     => $reqData["Token"],
+    "TxId"                      => $reqData["TxId"],
+    "UserName"                  => aesEncrypt($aesKey, $aesIv, $reqData["UserName"]),
+    "BirthDate"                 => aesEncrypt($aesKey, $aesIv, $reqData["BirthDate"]),
+    "UserCellphoneNumber"       => aesEncrypt($aesKey, $aesIv, $reqData["UserCellphoneNumber"]),
+    "기타필요한파라미터"            => "",
+);
+
+
+// API 호출
+$curl   = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL             => $url,
+    CURLOPT_RETURNTRANSFER  => true,
+    CURLOPT_CUSTOMREQUEST   => "POST",
+    CURLOPT_POSTFIELDS      => json_encode($bodies),
+    CURLOPT_HTTPHEADER      => $headers,
+    CURLOPT_VERBOSE         => false,
+    CURLOPT_SSL_VERIFYHOST  => 0,
+    CURLOPT_SSL_VERIFYPEER  => 0
+));
+
+$response   = curl_exec($curl);
+
+curl_close($curl);
+
+print($response);
+
+// 바이너리 파일 저장
+// API 마다 모델 구조가 다르니, 상황에 맞게 변경해주세요. (Result->BinaryData / Result->PdfData / Message / ...)
+file_put_contents("__BIN_FILEPATH__", base64_decode(json_decode($response)->Result->BinaryData));
+
+?>
